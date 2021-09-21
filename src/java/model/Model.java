5c54261960bee532585a4a1158d5df828eb07414
Your Name <you@example.com>
2021-09-21 23:03:21 +0700

    add model.java file

---
+++ src/java/model/Model.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Admin
+ */
+public class Model {
+    private static final String serverName = "ADMIN";
+    private static final String dbName = "orgasm";
+    private static final String portNumber = "1433";
+    private static final String user = "sa";
+    private static final String password = "root";
+    
+    public static Connection getConnection() throws Exception {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+            Connection conn = DriverManager.getConnection(url, user, password);
+            
+            return conn;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+}
