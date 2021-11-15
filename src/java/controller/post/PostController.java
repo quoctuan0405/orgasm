266b4cc9bbc8be868b766d9c7f01b44cb5ed3a59
DucTho2502 <dangductho1412@gmail.com>
2021-11-15 22:10:23 +0700

    complete post list

---
+++ src/java/controller/post/PostController.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.post;
+
+import controller.product.MyProductController;
+import java.io.IOException;
+import java.util.List;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.entity.Post;
+import model.dao.Posts;
+import model.entity.ProductCategory;
+import model.dao.ProductCategories;
+import model.dao.Users;
+import model.entity.User;
+
+/**
+ *
+ * @author dangd
+ */
+@WebServlet(name = "PostController", urlPatterns = {"/post"})
+public class PostController extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try {
+            HttpSession session = request.getSession();
+
+            int userId = (int) session.getAttribute("acc");
+
+            User user = Users.findById(userId);
+            request.setAttribute("user", user);
+            
+        } catch (Exception e) {
+            Logger.getLogger(PostController.class.getName()).log(Level.SEVERE, "Unauthorized", e);
+        }
+        
+        String page = request.getParameter("page");
+        if (page == null){
+            page = "1";
+        }
+        
+        int count = Posts.total();
+        int endPage = count / 5;
+        if (count % 5 != 0){
+            endPage++;
+        }
+        
+        List<Post> postList = null;
+        List<ProductCategory> categoryList = null;
+        
+        postList = Posts.paging(endPage);
+        
+        try {
+            categoryList = ProductCategories.allCategory();
+        } catch (SQLException ex) {
+            Logger.getLogger(PostController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                 
+        request.setAttribute("postList", postList);
+        request.setAttribute("categoryList", categoryList);
+        request.setAttribute("endPage", endPage);
+        request.setAttribute("currentPage", page);
+        request.getRequestDispatcher("Post.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
